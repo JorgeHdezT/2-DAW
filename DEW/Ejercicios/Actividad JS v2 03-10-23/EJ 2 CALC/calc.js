8edfa1c8037babc852785acb0b4abf9ec119433a
Hache <jorgeht2001@gmail.com>
2023-10-05 12:26:02 +0100

    5-10-23 AM
    calculadora dew

--- DEW/Ejercicios/Actividad JS v2 03-10-23/EJ 2 CALC/calc.js
+++ DEW/Ejercicios/Actividad JS v2 03-10-23/EJ 2 CALC/calc.js
@@ -10,23 +10,43 @@ function calculate() {
   try {
       let operation = document.getElementById('display').value;
     
-    if ((operation.split('^')).length == 2) {
-      const partes = operation.split('^');
-      const num1 = parseFloat(partes[0]);
-      const num2 = parseFloat(partes[1]);
-      const result = num1 ** num2;
-      document.getElementById('display').value = result;
-    } else {
-      const result = eval(operation);
-      document.getElementById('display').value = result;
-    }
+      if ((operation.split('^')).length == 2) {
+        const partes = operation.split('^');
+        const num1 = parseFloat(partes[0]);
+        const num2 = parseFloat(partes[1]);
+        const result = num1 ** num2;
+        document.getElementById('display').value = result;
+      }
+      else if (operation.split('%').length == 2) {
+        const partes = operation.split('%');
+        const num1 = parseFloat(partes[0]);
+        const num2 = parseFloat(partes[1]);
+        const result = ((num1*num2)/100);
+        document.getElementById('display').value = result;
+      }
+      else {
+        const result = eval(operation);
+        document.getElementById('display').value = result;
+      }
       
   } catch (error) {
       document.getElementById('display').value = 'Error';
   }
 }
-let expression = "2+5";
-const partes = expression.split('+');
+
+function generateRandom() {
+  // Genera un número aleatorio entre 0 y 1
+const numeroAleatorio = Math.random();
+
+// Escala el número aleatorio al rango deseado (0 a 100)
+const numeroEntre0y100 = Math.floor(numeroAleatorio * 101);
+appendToDisplay(numeroEntre0y100);
+
+}
+
+function calcSin() {
+  Math.sin();
+}
 
 
 
